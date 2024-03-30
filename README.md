44e18389a89f865aeee669adf5c20122f68f8f9b
vevisbeatriz <veronicablc@gmail.com>
2024-03-30 16:04:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# clima_clean_bloc_tdd
+ Weather app using clean architecure, BLoC and TDD
---
+++ android/app/src/main/kotlin/com/ricver/clima/clima_clean_bloc_tdd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ricver.clima.clima_clean_bloc_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
