5160cf3aaabce17d0af823167be750031e459a19
vevisbeatriz <veronicablc@gmail.com>
2024-03-31 18:38:42 -0300

    BLoC implementation + tests
    - BLoC implementation
    - BLoC test (initial state empty, state loaded when data is gotten successful and state failure when data is unsuccessful)

--- test/data/repositories/weather_repository_impl_test.dart
+++ test/data/repositories/weather_repository_impl_test.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:clima_clean_bloc_tdd/core/error/exception.dart';
 import 'package:clima_clean_bloc_tdd/core/error/failure.dart';
 import 'package:clima_clean_bloc_tdd/data/models/weather_model.dart';
 import 'package:clima_clean_bloc_tdd/data/repositories/weather_repository_impl.dart';
 import 'package:clima_clean_bloc_tdd/domain/entities/weather.dart';
-
-import 'weather_repository_impl_test.mocks.dart';
+import '../../helpers/test_helper.mocks.dart';
 
 void main() {
   late MockWeatherRemoteDataSource mockWeatherRemoteDataSource;
--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,9 +1,15 @@
+import 'package:clima_clean_bloc_tdd/data/data_sources/remote_data_source.dart';
+import 'package:clima_clean_bloc_tdd/domain/usecases/get_current_weather.dart';
 import 'package:mockito/annotations.dart';
 import 'package:clima_clean_bloc_tdd/domain/repositories/weather_repository.dart';
 import 'package:http/http.dart' as http;
 
 @GenerateMocks(
-  [WeatherRepository],
+  [
+    WeatherRepository,
+    WeatherRemoteDataSource,
+    GetCurrentWeatherUseCase,
+  ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
 void main() {
